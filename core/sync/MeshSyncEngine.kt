ce54e82d42a1fe1c2581133831c427266f41141d
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-22 23:12:46 -0400

    Actualizar MeshSyncEngine.kt

--- core/sync/MeshSyncEngine.kt
+++ core/sync/MeshSyncEngine.kt
@@ -1,1 +1,27 @@
+package core.sync
 
+enum class Transport { WifiDirect, LoRa }
+
+data class ThreatMessage(
+    val id: String,
+    val freqHz: Long,
+    val modulation: String,
+    val confidence: Double,
+    val timestamp: Long
+    // TODO: CBOR serialization
+)
+
+interface TransportAdapter {
+    fun start()
+    fun stop()
+    fun send(msg: ThreatMessage): Boolean
+    fun receive(handler: (ThreatMessage) -> Unit)
+}
+
+class MeshSyncEngine(
+    private val adapter: TransportAdapter
+) {
+    fun start(handler: (ThreatMessage) -> Unit) = adapter.start().also { adapter.receive(handler) }
+    fun stop() = adapter.stop()
+    fun share(threat: ThreatMessage): Boolean = adapter.send(threat)
+}
