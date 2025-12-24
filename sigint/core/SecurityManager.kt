4389c494107faa68674d13940b5a4ee144a990e0
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-24 16:05:40 -0400

    Actualizar SecurityManager.kt

--- sigint/core/SecurityManager.kt
+++ sigint/core/SecurityManager.kt
@@ -1,1 +1,8 @@
+package sigint.core
 
+import java.io.File
+
+object SecurityManager {
+    fun hasMissionKeys(): Boolean = File("data/keys/master.key").exists()
+    fun isGeofenceConfigured(): Boolean = File("core/security/active_geofence.poly").exists()
+}
