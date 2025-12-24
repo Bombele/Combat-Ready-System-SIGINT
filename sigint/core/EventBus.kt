63e2f46d33f5387b075b157bcc29694a7ff9778c
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-24 16:05:22 -0400

    Actualizar EventBus.kt

--- sigint/core/EventBus.kt
+++ sigint/core/EventBus.kt
@@ -1,1 +1,15 @@
+package sigint.core
 
+typealias Subscriber<T> = (T) -> Unit
+
+object EventBus {
+    private val subscribers = mutableMapOf<String, MutableList<Subscriber<Any>>>()
+
+    fun <T: Any> subscribe(topic: String, handler: Subscriber<T>) {
+        subscribers.getOrPut(topic) { mutableListOf() }.add(handler as Subscriber<Any>)
+    }
+
+    fun publish(topic: String, event: Any) {
+        subscribers[topic]?.forEach { it(event) }
+    }
+}
