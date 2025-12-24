3d7d71b931943c35e5de333d0b9b0a2721a744bc
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-24 16:00:44 -0400

    Actualizar SyncWorker.kt

--- sigint/sync/SyncWorker.kt
+++ sigint/sync/SyncWorker.kt
@@ -1,1 +1,25 @@
+package sigint.sync
+
+import core.sync.MeshSyncEngine
+import core.sync.UnifiedMessage
+import core.sync.MessageType
+import core.sync.PacketCodec
+import sigint.core.EventBus
+
+object SyncWorker {
+    fun startListening() {
+        // On s'abonne aux détections COMINT pour les envoyer dans le Mesh global
+        EventBus.subscribe<Map<String, Any>>("COMINT_DETECTION") { data ->
+            val threatPayload = PacketCodec.encodeCBOR(data)
+            
+            val msg = UnifiedMessage(
+                senderId = "SIGINT_UNIT_01",
+                type = MessageType.SIGINT_THREAT,
+                payload = threatPayload
+            )
+            
+            MeshSyncEngine.enqueueMessage(msg)
+        }
+    }
+}
 
