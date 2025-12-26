93091687e428121d52829e5e12a94f32b7d41b92
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-26 14:48:54 -0400

    Crear OffensiveBridge.kt

---
+++ src/main/kotlin/OffensiveBridge.kt
@@ -0,0 +1,10 @@
+package com.fardc.sigint.core
+
+class OffensiveBridge {
+    fun startFinancialInterception() {
+        println("🚀 Activation du module de Saisie Conservatoire Numérique...")
+        val process = ProcessBuilder("python3", "vectors/financial/auto_seizure.py")
+            .inheritIO()
+            .start()
+    }
+}
