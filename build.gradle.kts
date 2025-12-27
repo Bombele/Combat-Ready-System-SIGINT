7c830d4d49e16f0968f74a13c34de3db84ada53d
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-26 20:26:47 -0400

    Actualizar build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
-    // Versions synchronisées pour éviter l'erreur SelfResolvingDependency
-    kotlin("jvm") version "1.8.20" 
+    kotlin("jvm") version "1.8.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.fardc.sigint"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -13,20 +12,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    // Bibliothèques minimales pour la communication entre Kotlin et Python
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.shadowJar {
     archiveBaseName.set("sigint-core")
     archiveClassifier.set("all")
-    
     manifest {
-        // Définit le point d'entrée pour l'exécution du .jar
         attributes["Main-Class"] = "com.fardc.sigint.core.MainKt"
     }
 }
